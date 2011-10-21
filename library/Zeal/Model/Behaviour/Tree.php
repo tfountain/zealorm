8d87aa3a91e464c80e78cf36e1880045531ed8fa
Tim Fountain <tim@incutio.com>
2011-10-21 14:50:14 +0100

    Fixed primary key method call in tree behaviour, added some comments

--- library/Zeal/Model/Behaviour/Tree.php
+++ library/Zeal/Model/Behaviour/Tree.php
@@ -10,6 +10,21 @@
 
 class Zeal_Model_Behaviour_Tree extends Zeal_Model_BehaviourAbstract
 {
+    /**
+     * Returns the mapper being used by the tree
+     *
+     * By default this will use the mapper for the model class itself, but a different
+     * mapper can be specified using the 'mapper' option when setting up the tree:
+     *
+     *     public function init()
+     *     {
+     *         $this->actsAs('tree', array(
+     *             'mapper' => 'Application_Mapper_User
+     *         ));
+     *     }
+     *
+     * @return unknown_type
+     */
     public function getMapper()
     {
         if ($this->hasOption('mapper')) {
@@ -20,21 +35,37 @@ class Zeal_Model_Behaviour_Tree extends Zeal_Model_BehaviourAbstract
         }
     }
 
+    /**
+     * Returns the parent object for the model, loading it via. the model's mapper
+     *
+     * @return object
+     */
     public function parent()
     {
         return $this->getMapper()->find($this->getModel()->parentID);
     }
 
+    /**
+     * Returns whether or not the model has a parent
+     *
+     * @return boolean
+     */
     public function hasParent()
     {
         return ($this->getModel()->parentID > 0);
     }
 
+    /**
+     * Returns an array of child objects for this model
+     *
+     * @param false|string $order
+     * @return array
+     */
     public function children($order = false)
     {
         $mapper = $this->getMapper();
         $query = $mapper->query()
-            ->where('parentID = ?', $this->getModel()->{$mapper->getPrimaryKey()});
+            ->where('parentID = ?', $this->getModel()->{$mapper->getAdapter()->getPrimaryKey()});
 
         if ($order) {
             $query->order($order);
